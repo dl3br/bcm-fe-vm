9bde946e2d60b3ad24af1bf78d19ee4cf71d1cd7
Daniel Lebrecht <dlebrecht@gmail.com>
2018-02-14 18:07:05 +0100

    dbg

--- src/crossbar-relayer.ts
+++ src/crossbar-relayer.ts
@@ -1,13 +1,10 @@
-import { Observable } from 'rxjs'
 import { Client } from 'thruway.js'
 import { config } from '../config'
-import { Deal, MinsFromLastBlock } from './types'
 
-const topic = 'com.fee.deals'
 const topics = config.wamp.topics
 const wampRemote = new Client(config.wamp.remoteUrl, config.wamp.realm)
 const wampLocal = new Client(config.wamp.localUrl, config.wamp.realm)
 
 topics.map(topic => wampRemote.topic(topic)
-    .flatMap(y => y.args))
-    .forEach(val$ => wampLocal.publish(topic, val$))
+    .flatMap(y => y.args)
+    .forEach(val$ => wampLocal.publish(topic, val$)))
