3801b9ac3b0f7b7e71cfd30c24fd9efd56b5f258
Daniel Lebrecht <dlebrecht@gmail.com>
2018-02-23 01:30:37 +0100

    change port to 80

--- config.ts
+++ config.ts
@@ -1,6 +1,6 @@
 export const config = {
     wamp: {
-        localUrl: 'ws://localhost:8080/ws',
+        localUrl: 'ws://localhost/ws',
         remoteUrl: 'ws://185.19.28.13:8080/ws',
         realm: 'realm1',
         topics: ['com.fee.v1.btc.deals', 'com.fee.v1.btc.minsfromlastblock'],
