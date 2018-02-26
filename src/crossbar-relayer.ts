8b390d8e081a4beb72403dda53c9c8195a5da5a1
Daniel Lebrecht <dlebrecht@gmail.com>
2018-02-26 18:50:07 +0100

    relayer

--- src/crossbar-relayer.ts
+++ src/crossbar-relayer.ts
@@ -5,6 +5,9 @@ const topics = config.wamp.topics
 const wampRemote = new Client(config.wamp.remoteUrl, config.wamp.realm)
 const wampLocal = new Client(config.wamp.localUrl, config.wamp.realm)
 
-topics.map(topic => wampRemote.topic(topic)
-    .flatMap(y => y.args)
-    .forEach(val$ => wampLocal.publish(topic, val$)))
+const data = topics.map(topic => wampRemote.topic(topic)
+    .map(y => ({ val: y.args, topic })))
+
+data.map(x => x.subscribe(
+    ({val, topic}) => wampLocal.publish(topic, val)
+))
