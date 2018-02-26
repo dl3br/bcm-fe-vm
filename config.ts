6e65647d8b33dbb5c1447c2286a19ac0aa0cbf02
Daniel Lebrecht <dlebrecht@gmail.com>
2018-02-26 18:06:49 +0100

    change ip

--- config.ts
+++ config.ts
@@ -1,7 +1,7 @@
 export const config = {
     wamp: {
         localUrl: 'ws://localhost/ws',
-        remoteUrl: 'ws://185.19.28.13:8080/ws',
+        remoteUrl: 'ws://159.100.240.25:8080/ws',
         realm: 'realm1',
         topics: ['com.fee.v1.btc.deals', 'com.fee.v1.btc.minsfromlastblock'],
     }
