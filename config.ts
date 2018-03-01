77e4f2fd716bd49ce7790d9369545918ccb32d39
Daniel Lebrecht <dlebrecht@gmail.com>
2018-03-01 17:58:53 +0100

    making crossbar private and adding challenge to publish

--- config.ts
+++ config.ts
@@ -1,8 +1,10 @@
 export const config = {
     wamp: {
         localUrl: 'ws://localhost:8080/ws',
-        remoteUrl: 'ws://159.100.240.25:8080/ws',
+        remoteUrl: 'ANSIBLE_ME',
         realm: 'realm1',
+        user: "relayer",
+        key: 'ANSIBLE_ME',
         topics: ['com.fee.v1.btc.deals', 'com.fee.v1.btc.minsfromlastblock'],
     }
 }
--- src/crossbar-relayer.ts
+++ src/crossbar-relayer.ts
@@ -1,9 +1,21 @@
 import { Client } from 'thruway.js'
 import { config } from '../config'
+import { auth_cra } from 'autobahn'
 
 const topics = config.wamp.topics
 const wampRemote = new Client(config.wamp.remoteUrl, config.wamp.realm)
-const wampLocal = new Client(config.wamp.localUrl, config.wamp.realm)
+const wampLocal = new Client(
+    config.wamp.localUrl,
+    config.wamp.realm,
+    {
+        authmethods: ['wampcra'],
+        // role: config.wamp.user,
+        authid: config.wamp.user,
+    }
+)
+
+wampLocal.onChallenge(challenge => challenge
+    .map(({ extra }) => auth_cra.sign(config.wamp.key, extra.challenge)))
 
 topics.map(topic => wampRemote.topic(topic)
     .flatMap(y => y.args)
